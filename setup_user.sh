17f2a5966346c84b771c78a3bfaafc6370f8ef14
Geoff Williams <geoff@declarativesystems.com>
2023-11-30 11:40:45 +1100

    script outputs

--- setup_user.sh
+++ setup_user.sh
@@ -5,6 +5,7 @@ mkdir -p ~/bin
 
 # kubectl - check our own version as WSL2 docker installs one to /usr/local/bin
 if [ ! -f ~/bin/kubectl ] ; then
+    echo "installing kubectl..."
     pushd ~/bin
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x kubectl
@@ -14,12 +15,14 @@ fi
 
 # helm
 if [ -z $(which helm) ] ; then
+    echo "installing helm..."
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | HELM_INSTALL_DIR=~/bin USE_SUDO=false bash
     echo 'source <(helm completion bash)' >> ~/.profile
 fi
 
 # Maven
 if [ -z $(which mvn) ] ; then
+    echo "installing maven..."
     pushd ~/
     MAVEN_VERSION="3.9.5"
     curl -LO https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
@@ -31,6 +34,7 @@ fi
 
 # Gradle
 if [ -z $(which gradle) ] ; then
+    echo "installing gradle..."
     pushd ~/
     GRADLE_VERSION="8.4"
     curl -LO https://services.gradle.org/distributions/gradle-8.4-bin.zip
@@ -45,6 +49,7 @@ CONFLUENT_MAJOR_VERSION="7.5"
 CONFLUENT_FULL_VERSION="${CONFLUENT_MAJOR_VERSION}.2"
 CONFLUENT_SYMLINK=~/confluent
 if [ ! -d ${CONFLUENT_SYMLINK} ] ; then
+    echo "installing confluent sdk..."
     pushd ~/
     curl -O https://packages.confluent.io/archive/${CONFLUENT_MAJOR_VERSION}/confluent-${CONFLUENT_FULL_VERSION}.zip
     unzip confluent-${CONFLUENT_FULL_VERSION}.zip
@@ -55,6 +60,7 @@ fi
 # confluent CLI
 # confluent SDK normally ships an old CLI so ignore it...
 if [ ! -f ~/bin/confluent ] ; then
+    echo "installing confluent cli..."
     pushd ~/
     curl -sL --http1.1 https://cnfl.io/cli | sh -s -- latest
     mkdir -p ~/.confluent
@@ -65,6 +71,7 @@ fi
 
 # python env (different version of python)
 if [ -z $(which pyenv) ] ; then
+    echo "installing pyenv..."
     curl https://pyenv.run | bash
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
     echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
@@ -88,6 +95,7 @@ fi
 if [ -z $(which pip ) ]  ; then
     echo "rerun this script after installing python to install xdg-open"
 elif [ -z $(which xdg-open) ] ; then
+    echo "installing xdg-open-wsl..."
     # install xdg-open-wsl using your latest pip
     pip install --user git+https://github.com/cpbotha/xdg-open-wsl.git
     echo "RESTART YOUR SHELL TO ENABLE xdg-open"
@@ -95,11 +103,13 @@ fi
 
 # k3d (kubernetes in docker)
 if [ -z $(which k3d) ] ; then
+    echo "installing k3d..."
     curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | K3D_INSTALL_DIR=~/bin USE_SUDO=false bash
 fi
 
 # istioctl
 if [ ! -f ~/.istioctl/bin/istioctl ] ; then
+    echo "installing istioctl..."
     curl -sL https://istio.io/downloadIstioctl | sh -
     echo 'export PATH=$HOME/.istioctl/bin:$PATH' >> ~/.profile
     # shell completion needs full install of istio... im not that interested
@@ -107,11 +117,13 @@ fi
 
 # ~/.vimrc
 if [ ! -f ~/.vimrc ] ; then
+    echo "configure vim..."
     cp files/dot.vimrc ~/.vimrc
 fi
 
 # nicer shell prompt with kubernetes info
 if ! grep kubernetes_prompt.sh ~/.profile ; then
+    echo "configure prompt..."
     cp files/kubernetes_prompt.sh ~/bin
     echo 'source ~/bin/kubernetes_prompt.sh' >> ~/.profile
 fi
